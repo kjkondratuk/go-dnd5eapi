63f10c642c8a3bd0f666a62ba6e7f177a0460543
Kevin Kondratuk <paintface07@gmail.com>
2020-09-12 18:37:43 -0500

    add some functionality around classes and proficiencies

--- dnd5eapiclient.go
+++ dnd5eapiclient.go
@@ -5,33 +5,41 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
-	"strings"
 
 	"github.com/kjkondratuk/go-dnd5eapi/constants"
 	"github.com/kjkondratuk/go-dnd5eapi/response"
 )
 
-type apiClient struct {
-	client  *http.Client
-	baseURL string
-}
+type (
+	apiClient struct {
+		client  *http.Client
+		baseURL string
+	}
 
-type ApiClient interface {
-	// Endpoints
-	GetEndpointList() (*response.EndpointResponse, error)
+	ApiClient interface {
+		// Endpoints
+		GetEndpointList() (*response.EndpointResponse, error)
 
-	// Ability Score
-	GetAbilityScoreList() (*response.ListResponse, error)
-	GetAbilityScoreByName(name string) (*response.AbilityScoreDetail, error)
+		// Ability Score
+		GetAbilityScoreList() (*response.ListResponse, error)
+		GetAbilityScoreByIndex(index string) (*response.AbilityScoreDetail, error)
 
-	// Skills
-	GetSkillList() (*response.ListResponse, error)
-	GetSkillByName(name string) (*response.SkillDetail, error)
+		// Skills
+		GetSkillList() (*response.ListResponse, error)
+		GetSkillByIndex(index string) (*response.SkillDetail, error)
 
-	// Skill <-> Ability Score
-	GetAbilityScoreForSkill(skillName string) (*response.AbilityScoreDetail, error)
-	GetSkillsForAbilityScore(name string) ([]response.SkillDetail, error)
-}
+		// Skill <-> Ability Score
+		GetAbilityScoreForSkillByIndex(skillIndex string) (*response.AbilityScoreDetail, error)
+		GetSkillsForAbilityByIndex(index string) ([]response.SkillDetail, error)
+
+		// Class
+		GetClassList() (*response.ListResponse, error)
+		GetClassByIndex(index string) (*response.ClassDetail, error)
+
+		// Class <-> Proficiencies
+		GetProficienciesForClassByIndex(classIndex string) ([]response.ProficiencyDetail, error)
+	}
+)
 
 // region ApiClient Implementation
 func NewApiClient(baseURL string, opts ...ClientOption) ApiClient {
@@ -47,6 +55,7 @@ func NewApiClient(baseURL string, opts ...ClientOption) ApiClient {
 	return client
 }
 
+// region private functions
 func (ac *apiClient) apiGet(uri string) ([]byte, error) {
 	resp, err := ac.client.Get(ac.baseURL + uri)
 	if err != nil {
@@ -81,7 +90,9 @@ func (ac *apiClient) getListForUrl(url string) (*response.ListResponse, error) {
 
 // endregion
 
-// region Options
+// endregion
+
+// region Client Options
 type ClientOption func(*apiClient)
 
 func WithHttpClient(client *http.Client) ClientOption {
@@ -114,8 +125,8 @@ func (ac *apiClient) GetAbilityScoreList() (*response.ListResponse, error) {
 	return ac.getListForUrl(constants.AbilityScoreEndpoint)
 }
 
-func (ac *apiClient) GetAbilityScoreByName(name string) (*response.AbilityScoreDetail, error) {
-	result, err := ac.apiGet(constants.AbilityScoreChildEndpoint + strings.ToLower(name))
+func (ac *apiClient) GetAbilityScoreByIndex(index string) (*response.AbilityScoreDetail, error) {
+	result, err := ac.apiGet(constants.AbilityScoreChildEndpoint + index)
 	if err != nil {
 		return nil, err
 	}
@@ -128,26 +139,6 @@ func (ac *apiClient) GetAbilityScoreByName(name string) (*response.AbilityScoreD
 	return &d, nil
 }
 
-func (ac *apiClient) GetSkillsForAbilityScore(name string) ([]response.SkillDetail, error) {
-	ability, err := ac.GetAbilityScoreByName(name)
-	if err != nil {
-		return nil, err
-	}
-
-	var result []response.SkillDetail
-	for _, v := range ability.Skills {
-		skill, err := ac.GetSkillByName(v.Index)
-		if err != nil {
-			return nil, err
-		}
-		if skill != nil {
-			result = append(result, *skill)
-		} else {
-			return nil, errors.New("No skill called " + v.Index + " could be found")
-		}
-	}
-	return result, nil
-}
 // endregion
 
 // region Skills
@@ -156,8 +147,8 @@ func (ac *apiClient) GetSkillList() (*response.ListResponse, error) {
 	return ac.getListForUrl(constants.SkillsEndpoint)
 }
 
-func (ac * apiClient) GetSkillByName(name string) (*response.SkillDetail, error) {
-	result, err := ac.apiGet(constants.SkillsChildEndpoint + strings.ReplaceAll(strings.ToLower(name), " ", "-"))
+func (ac *apiClient) GetSkillByIndex(index string) (*response.SkillDetail, error) {
+	result, err := ac.apiGet(constants.SkillsChildEndpoint + index)
 	if err != nil {
 		return nil, err
 	}
@@ -170,12 +161,36 @@ func (ac * apiClient) GetSkillByName(name string) (*response.SkillDetail, error)
 	return &d, nil
 }
 
-func (ac *apiClient) GetAbilityScoreForSkill(skillName string) (*response.AbilityScoreDetail, error) {
-	skill, err := ac.GetSkillByName(skillName)
+// endregion
+
+// region Skill <-> Ability Score
+func (ac *apiClient) GetSkillsForAbilityByIndex(index string) ([]response.SkillDetail, error) {
+	ability, err := ac.GetAbilityScoreByIndex(index)
 	if err != nil {
 		return nil, err
 	}
-	abilityScore, err := ac.GetAbilityScoreByName(skill.AbilityScore.Name)
+
+	var result []response.SkillDetail
+	for _, v := range ability.Skills {
+		skill, err := ac.GetSkillByIndex(v.Index)
+		if err != nil {
+			return nil, err
+		}
+		if skill != nil {
+			result = append(result, *skill)
+		} else {
+			return nil, errors.New("No skill called " + v.Index + " could be found")
+		}
+	}
+	return result, nil
+}
+
+func (ac *apiClient) GetAbilityScoreForSkillByIndex(skillIndex string) (*response.AbilityScoreDetail, error) {
+	skill, err := ac.GetSkillByIndex(skillIndex)
+	if err != nil {
+		return nil, err
+	}
+	abilityScore, err := ac.GetAbilityScoreByIndex(skill.AbilityScore.Index)
 	if err != nil {
 		return nil, err
 	}
@@ -187,6 +202,86 @@ func (ac *apiClient) GetAbilityScoreForSkill(skillName string) (*response.Abilit
 // region Classes
 func (ac *apiClient) GetClassList() (*response.ListResponse, error) {
 	return ac.getListForUrl(constants.ClassEndpoint)
+}
+
+func (ac *apiClient) GetClassByIndex(index string) (*response.ClassDetail, error) {
+	result, err := ac.apiGet(constants.ClassChildEndpoint + index)
+	if err != nil {
+		return nil, err
+	}
+
+	d := response.ClassDetail{}
+	err = json.Unmarshal(result, &d)
+	if err != nil {
+		return nil, err
+	}
+	return &d, nil
+}
+
+// endregion
+
+// region Classes <-> Proficiency
+func (ac *apiClient) GetProficienciesForClassByIndex(classIndex string) ([]response.ProficiencyDetail, error) {
+	class, err := ac.GetClassByIndex(classIndex)
+	if err != nil {
+		return nil, err
+	}
+
+	var result []response.ProficiencyDetail
+	for _, v := range class.Proficiencies {
+		prof, err := ac.GetProficiencyByIndex(v.Index)
+		if err != nil {
+			return nil, err
+		}
+		if prof != nil {
+			result = append(result, *prof)
+		} else {
+			return nil, errors.New("No proficiency called " + v.Index + " could be found")
+		}
+	}
+	return result, nil
+}
+
+func (ac *apiClient) GetProficiencyChoicesForClassByIndex(classIndex string) (map[string][]response.ProficiencyDetail, error) {
+	class, err := ac.GetClassByIndex(classIndex)
+	if err != nil {
+		return nil, err
+	}
+
+	var result = make(map[string][]response.ProficiencyDetail)
+	for i, choice := range class.ProficiencyChoices {
+		var profs []response.ProficiencyDetail
+		for _, prof := range class.ProficiencyChoices[i].From {
+			p, err := ac.GetProficiencyByIndex(prof.Index)
+			if err != nil {
+				return nil, err
+			}
+			if p != nil {
+				profs = append(profs, *p)
+			} else {
+				return nil, errors.New("No proficiency called " + prof.Index + " could be found")
+			}
+		}
+		result[choice.Type] = profs
+	}
+	return result, nil
+}
+
+// endregion
+
+//region Proficiencies
+func (ac *apiClient) GetProficiencyByIndex(index string) (*response.ProficiencyDetail, error) {
+	result, err := ac.apiGet(constants.ProficienciesChildEndpoint + index)
+	if err != nil {
+		return nil, err
+	}
+
+	d := response.ProficiencyDetail{}
+	err = json.Unmarshal(result, &d)
+	if err != nil {
+		return nil, err
+	}
+	return &d, nil
 }
 
 // endregion
--- response/responses.go
+++ response/responses.go
@@ -10,11 +10,11 @@ type (
 	}
 
 	AbilityScoreDetail struct {
-		Index       string         `json:"index"`
-		Name        string         `json:"name"`
-		Url         string         `json:"url"`
-		FullName    string         `json:"full_name"`
-		Description []string       `json:"desc"`
+		Index       string   `json:"index"`
+		Name        string   `json:"name"`
+		Url         string   `json:"url"`
+		FullName    string   `json:"full_name"`
+		Description []string `json:"desc"`
 		Skills      []APIRef `json:"skills"`
 	}
 
@@ -22,28 +22,37 @@ type (
 
 	// region Skills
 	SkillDetail struct {
-		Index        string `json:"index"`
-		Name         string `json:"name"`
-		Url          string `json:"url"`
+		Index        string   `json:"index"`
+		Name         string   `json:"name"`
+		Url          string   `json:"url"`
 		Description  []string `json:"desc"`
-		AbilityScore APIRef `json:"ability_score"`
+		AbilityScore APIRef   `json:"ability_score"`
 	}
 
 	ListResponse struct {
-		Count int `json:"count"`
+		Count   int `json:"count"`
 		Results []APIRef
 	}
 
 	// endregion
 
+	// region Class
 	ClassDetail struct {
-		Index              string              `json:"index"`
-		Name               string              `json:"name"`
-		Url                string              `json:"url"`
-		HitDie             int                 `json:"hit_die"`
-		ProficiencyChoices []ProficiencyChoice `json:"proficiency_choices"`
+		Index                 string              `json:"index"`
+		Name                  string              `json:"name"`
+		Url                   string              `json:"url"`
+		HitDie                int                 `json:"hit_die"`
+		ProficiencyChoices    []ProficiencyChoice `json:"proficiency_choices"`
+		Proficiencies         []APIRef            `json:"proficiencies"`
+		SavingThrows          APIRef              `json:"saving_throws"`
+		StartingEquipmentLink string              `json:"starting_equipment"`
+		ClassLevelsLink       string              `json:"class_levels"`
+		Subclasses            []APIRef            `json:"subclasses"`
 	}
 
+	// endregion
+
+	// region Proficiency
 	ProficiencyChoice struct {
 		Choose int      `json:"choose"`
 		Type   string   `json:"type"`
@@ -59,4 +68,5 @@ type (
 		Races      []APIRef `json:"races"`
 		References []APIRef `json:"references"`
 	}
+	// endregion
 )
