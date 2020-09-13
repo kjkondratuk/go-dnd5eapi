b86411693eefe66b52edd53a5cad9e10622a2609
Kevin Kondratuk <paintface07@gmail.com>
2020-09-12 20:51:12 -0500

    add models for languages, subraces, and traits

--- response/subraces.go
+++ response/subraces.go
@@ -2,11 +2,13 @@ package response
 
 type (
 	Subrace struct {
-		Index                 string         `json:"index"`
-		Name                  string         `json:"name"`
-		Url                   string         `json:"url"`
-		AbilityBonuses        []AbilityBonus `json:"ability_bonuses"`
-		StartingProficiencies []APIRef       `json:"starting_proficiencies"`
-		Languages             []APIRef       `json:"languages"`
+		Index                 string           `json:"index"`
+		Name                  string           `json:"name"`
+		Url                   string           `json:"url"`
+		AbilityBonuses        []AbilityBonus   `json:"ability_bonuses"`
+		StartingProficiencies []APIRef         `json:"starting_proficiencies"`
+		Languages             []APIRef         `json:"languages"`
+		LanguageChoice        []LanguageChoice `json:"language_options"`
+		RacialTraits          []Trait          `json:"racial_traits"`
 	}
 )
--- response/traits.go
+++ response/traits.go
@@ -1,6 +1,14 @@
 package response
 
 type (
+	Trait struct {
+		Index    string   `json:"index"`
+		Name     string   `json:"name"`
+		Url      string   `json:"url"`
+		Races    []APIRef `json:"races"`
+		Subraces []APIRef `json:"subraces"`
+	}
+
 	TraitChoice struct {
 		Choose int      `json:"choose"`
 		Type   string   `json:"type"`
