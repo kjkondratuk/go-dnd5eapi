f0879de2e918c2bb557585665198ada82c42f9e0
Kevin Kondratuk <paintface07@gmail.com>
2020-09-26 15:57:08 -0500

    add some of the clients via the main API client

--- dnd5eapiclient.go
+++ dnd5eapiclient.go
@@ -4,8 +4,15 @@ import (
 	"github.com/kjkondratuk/go-dnd5eapi/ability_scores"
 	"github.com/kjkondratuk/go-dnd5eapi/api"
 	"github.com/kjkondratuk/go-dnd5eapi/classes"
+	classes_proficiency_rel "github.com/kjkondratuk/go-dnd5eapi/classes_proficiencies_rel"
+	"github.com/kjkondratuk/go-dnd5eapi/conditions"
+	"github.com/kjkondratuk/go-dnd5eapi/damage"
 	"github.com/kjkondratuk/go-dnd5eapi/endpoints"
+	"github.com/kjkondratuk/go-dnd5eapi/equipment"
+	"github.com/kjkondratuk/go-dnd5eapi/features"
 	"github.com/kjkondratuk/go-dnd5eapi/languages"
+	"github.com/kjkondratuk/go-dnd5eapi/magic_schools"
+	"github.com/kjkondratuk/go-dnd5eapi/monsters"
 	"github.com/kjkondratuk/go-dnd5eapi/proficiencies"
 	"github.com/kjkondratuk/go-dnd5eapi/races"
 	"github.com/kjkondratuk/go-dnd5eapi/skills"
@@ -14,28 +21,44 @@ import (
 
 type (
 	Client struct {
-		basicsProvider api.BasicsProvider
-		AbilityScores  ability_scores.AbilityScoreClient
-		Classes        classes.ClassesClient
-		Endpoints      endpoints.EndpointsClient
-		Languages      languages.LanguageClient
-		Proficiencies  proficiencies.ProficiencyClient
-		Races          races.RaceClient
-		Skills         skills.SkillClient
-		Subclasses     subclasses.SubclassClient
+		basicsProvider    api.BasicsProvider
+		AbilityScores     ability_scores.AbilityScoreClient
+		Classes           classes.ClassesClient
+		ClassProficencies classes_proficiency_rel.ClassProficiencyRelClient
+		Conditions        conditions.ConditionClient
+		Damage            damage.DamageTypeClient
+		Endpoints         endpoints.EndpointsClient
+		Equipment         equipment.EquipmentClient
+		Features          features.FeatureClient
+		Languages         languages.LanguageClient
+		MagicSchools      magic_schools.MagicSchoolsClient
+		Monsters          monsters.MonsterClient
+		Proficiencies     proficiencies.ProficiencyClient
+		Races             races.RaceClient
+		Skills            skills.SkillClient
+		Subclasses        subclasses.SubclassClient
 	}
 )
 
 func NewClient(basicsProvider api.BasicsProvider) *Client {
-	return &Client{
-		basicsProvider: basicsProvider,
-		AbilityScores:  ability_scores.NewClient(basicsProvider),
-		Classes:        classes.NewClient(basicsProvider),
-		Endpoints:      endpoints.NewClient(basicsProvider),
-		Languages:      languages.NewClient(basicsProvider),
-		Proficiencies:  proficiencies.NewClient(basicsProvider),
-		Races:          races.NewClient(basicsProvider),
-		Skills:         skills.NewClient(basicsProvider),
-		Subclasses:     subclasses.NewClient(basicsProvider),
+	client := &Client{
+		basicsProvider:    basicsProvider,
+		AbilityScores:     ability_scores.NewClient(basicsProvider),
+		Classes:           classes.NewClient(basicsProvider),
+		ClassProficencies: nil,
+		Conditions:        conditions.NewClient(basicsProvider),
+		Damage:            damage.NewClient(basicsProvider),
+		Endpoints:         endpoints.NewClient(basicsProvider),
+		Equipment:         equipment.NewClient(basicsProvider),
+		Features:          features.NewClient(basicsProvider),
+		Languages:         languages.NewClient(basicsProvider),
+		MagicSchools:      magic_schools.NewClient(basicsProvider),
+		Monsters:          monsters.NewClient(basicsProvider),
+		Proficiencies:     proficiencies.NewClient(basicsProvider),
+		Races:             races.NewClient(basicsProvider),
+		Skills:            skills.NewClient(basicsProvider),
+		Subclasses:        subclasses.NewClient(basicsProvider),
 	}
+	client.ClassProficencies = classes_proficiency_rel.NewClient(client.Classes, client.Proficiencies)
+	return client
 }
