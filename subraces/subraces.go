327cee71fb638f7ee88ad9b189eaf65c2249c96a
Kevin Kondratuk <paintface07@gmail.com>
2020-10-11 12:02:55 -0500

    add subraces endpoints

---
+++ subraces/subraces.go
@@ -0,0 +1,24 @@
+//go:generate go run ../gen/api_gen.go ../gen subraces Subrace SubraceDetail "\"high-elf\""
+
+package subraces
+
+import (
+	"github.com/kjkondratuk/go-dnd5eapi/ability_scores"
+	"github.com/kjkondratuk/go-dnd5eapi/api"
+)
+
+type (
+	SubraceDetail struct {
+		Index                 string                        `json:"index"`
+		Name                  string                        `json:"name"`
+		Url                   string                        `json:"url"`
+		Race                  api.Ref                       `json:"race"`
+		Description           string                        `json:"desc"`
+		AbilityBonuses        []ability_scores.AbilityBonus `json:"ability_bonuses"`
+		StartingProficiencies []api.Ref                     `json:"starting_proficiencies"`
+		Languages             []api.Ref                     `json:"languages"`
+		LanguageOptions       api.Choice                    `json:"language_options"`
+		RacialTraits          []api.Ref                     `json:"racial_traits"`
+		RacialTraitOptions    api.Choice                    `json:"racial_trait_options"`
+	}
+)
