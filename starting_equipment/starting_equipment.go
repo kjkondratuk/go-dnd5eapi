d2dfb779a2f4a57cf4f38e60cfcb09ff5d38efaa
Kevin Kondratuk <paintface07@gmail.com>
2020-10-11 11:52:37 -0500

    add starting_equipment endpoints

---
+++ starting_equipment/starting_equipment.go
@@ -0,0 +1,21 @@
+//go:generate go run ../gen/api_gen.go ../gen starting_equipment StartingEquipment StartingEquipmentDetail "\"barbarian\""
+
+package starting_equipment
+
+import "github.com/kjkondratuk/go-dnd5eapi/api"
+
+type (
+	StartingEquipmentDetail struct {
+		Index                    string          `json:"index"`
+		Name                     string          `json:"name"`
+		Url                      string          `json:"url"`
+		Class                    api.Ref         `json:"class"`
+		StartingEquipment        []EquipmentItem `json:"starting_equipment"`
+		StartingEquipmentOptions []api.Choice    `json:"starting_equipment_options"`
+	}
+
+	EquipmentItem struct {
+		Equipment api.Ref `json:"equipment"`
+		Quantity  int     `json:"quantity"`
+	}
+)
